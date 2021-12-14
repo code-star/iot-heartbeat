08f7ad7eeda00fd30812c364901e466fd3149950
mdvanes <es@mdworld.nl>
2021-12-14 10:10:38 +0100

    Add styles

--- src/StatusCard/StatusCard.tsx
+++ src/StatusCard/StatusCard.tsx
@@ -1,46 +1,11 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { CircularProgress } from '@mui/material';
-import { flexbox } from '@mui/system';
-
-// const bull = (
-//     <Box
-//         component="span"
-//         sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-//     >
-//         •
-//     </Box>
-// );
-
-// const card = (
-//     <React.Fragment>
-//         <CardContent>
-//             <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-//                 NUC 11443323
-//             </Typography>
-//             <Typography variant="h5" component="div">
-//                 IP 10.0.100.1
-//             </Typography>
-//             <Typography sx={{ mb: 1.5 }} color="text.secondary">
-//                 Location: Westfields Oirschot
-//             </Typography>
-//             <Typography variant="body2">
-//                 serialnr 2454ae245afh141145
-//                 <br />
-//                 type NUC
-//             </Typography>
-//             <CircularProgress color="secondary" />
-//         </CardContent>
-//         {/* <CardActions>
-//             <Button size="small">Learn More</Button>
-//         </CardActions> */}
-//     </React.Fragment>
-// );
+import { CircularProgress, Grid, IconButton } from '@mui/material';
+import { Alarm as AlarmIcon, Check as CheckIcon } from '@mui/icons-material';
 
 const wait = (delay: number) => new Promise((resolve) => {
     setTimeout(() => { resolve("done") }, delay);
@@ -56,6 +21,7 @@ export default function StatusCard() {
     const [isLoading, setIsLoading] = React.useState(false);
     const [deviceState, setDeviceState] = React.useState(false);
     const handleClick = async () => {
+        await wait(5000);
         setIsLoading(true);
         setDeviceState(true);
         await wait(1000);
@@ -67,22 +33,34 @@ export default function StatusCard() {
                 <CardContent style={{ height: "100%" }}>
                     {isLoading ? <Loader /> :
                         <>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom style={{ fontSize: "160%" }}>
                                 NUC 11443323
                             </Typography>
-                            <Typography variant="h5" component="div">
+                            <Typography variant="h5" component="div" style={{ fontSize: "300%" }}>
                                 IP 10.0.100.1
                             </Typography>
-                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                            {/* <Typography gutterBottom>
+                                <strong>{deviceState ? "CONNECTED" : "DISCONNECTED"}</strong>
+                            </Typography> */}
+                            <Grid container gap={2} my={4}>
+                                <Grid item>
+                                    {/* button (pulse)
+                                                          */}
+                                    <Button variant="contained" color="success" size="large">
+                                        <CheckIcon />
+                                    </Button>
+                                </Grid>
+                                <Grid item xs>
+                                    <Button variant="outlined" fullWidth size="large">{deviceState ? "CONNECTED" : "DISCONNECTED"}</Button>
+                                </Grid>
+                            </Grid>
+                            <Typography sx={{ mb: 1.5 }} color="text.secondary" style={{ fontSize: "160%" }}>
                                 Location: Westfields Oirschot
                             </Typography>
-                            <Typography variant="body2" gutterBottom>
+                            <Typography variant="body2" gutterBottom style={{ fontSize: "140%" }}>
                                 serialnr 2454ae245afh141145
                                 <br />
                                 type NUC
-                            </Typography>
-                            <Typography>
-                                <strong>{deviceState ? "CONNECTED" : "DISCONNECTED"}</strong>
                             </Typography>
                         </>}
                 </CardContent>
